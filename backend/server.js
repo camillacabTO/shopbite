f8a3cbb067d84b2721ffb8e3fe581fa6c481e445
Camila <camillacab@hotmail.com>
2023-02-28 11:17:10 -0500

    test for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,39 @@
 // import express from 'express'
-import express from 'express';
-import dotenv from 'dotenv';
-import connectToDB from './database/db.js';
-import productRoutes from './routes/productsRoutes.js';
-import userRoutes from './routes/usersRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import { notFound, errorHandler } from './middleware/error.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+import connectToDB from './database/db.js'
+import productRoutes from './routes/productsRoutes.js'
+import userRoutes from './routes/usersRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import { notFound, errorHandler } from './middleware/error.js'
 
-dotenv.config();
-connectToDB();
+dotenv.config()
+connectToDB()
+const app = express()
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(express.json())
 
-app.use(express.json());
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+const __dirname = path.resolve()
 
-app.use(notFound);
-app.use(errorHandler);
-// delete this?
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-app.listen(PORT, () =>
-  console.log(
-    `Server running in ${process.env.NODE_ENV}, listening on port ${PORT}`
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   )
-);
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
